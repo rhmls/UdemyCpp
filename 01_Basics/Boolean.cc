2444de8d9b5f8fb9420d19b3823455e1af2ba100
rhmls <rahmel.swante@roeders.de>
2022-06-14 12:36:07 +0200

    Test123

--- 01_Basics/Boolean.cc
+++ 01_Basics/Boolean.cc
@@ -10,7 +10,7 @@ int main()
     std::cin >> number;
 
     bool check = (number >= 0 && number <= 10);
-    std::cout << std::boolalpha << check << std::endl;
+    std::cout << std::boolalpha << check << std::endl; //
 
     bool check2 = !(number >= 0 && number <= 10);
     std::cout << std::boolalpha << check2 << std::endl;
